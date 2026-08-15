07826fbe7fa69aca1344b7025fcc18a110ac1d9a
Aryan Raj Verma <aryanrajverma03@gmail.com>
2026-08-15 22:34:07 +0530

    Time: 11 ms (96.65%), Space: 47.4 MB (25.3%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,68 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (s.length() == 0 || words.length == 0) {
+            return ans;
+        }
+
+        int wordLen = words[0].length();
+        int totalWords = words.length;
+        int totalLen = wordLen * totalWords;
+
+        HashMap<String, Integer> required = new HashMap<>();
+
+        for (String word : words) {
+            required.put(word, required.getOrDefault(word, 0) + 1);
+        }
+
+        for (int offset = 0; offset < wordLen; offset++) {
+
+            int left = offset;
+            int count = 0;
+
+            HashMap<String, Integer> seen = new HashMap<>();
+
+            for (int right = offset;
+                 right + wordLen <= s.length();
+                 right += wordLen) {
+
+                String word = s.substring(right, right + wordLen);
+
+                if (!required.containsKey(word)) {
+                    seen.clear();
+                    count = 0;
+                    left = right + wordLen;
+                    continue;
+                }
+
+                seen.put(word, seen.getOrDefault(word, 0) + 1);
+                count++;
+
+                while (seen.get(word) > required.get(word)) {
+
+                    String leftWord = s.substring(left, left + wordLen);
+
+                    seen.put(leftWord, seen.get(leftWord) - 1);
+
+                    left += wordLen;
+                    count--;
+                }
+
+                if (count == totalWords) {
+                    ans.add(left);
+
+                    String leftWord = s.substring(left, left + wordLen);
+
+                    seen.put(leftWord, seen.get(leftWord) - 1);
+
+                    left += wordLen;
+                    count--;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
